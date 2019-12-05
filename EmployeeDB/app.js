93bb6edb8f34dc4ac8780ffee568da1fbed5c938
samdesh-gcp1 <58552071+samdesh-gcp1@users.noreply.github.com>
2019-12-05 16:54:52 +0530

    Update app.js

--- EmployeeDB/app.js
+++ EmployeeDB/app.js
@@ -65,7 +65,7 @@ var employeeProvider= new EmployeeProvider('localhost', 27017);
 //Routes
 
 //index
-app.get('/', function(req, res){
+app.get('/sample-node-mongo-app/', function(req, res){
   employeeProvider.findAll(function(error, emps){
       res.render('index', {
             title: 'Employees',
@@ -75,24 +75,24 @@ app.get('/', function(req, res){
 });
 
 //new employee
-app.get('/employee/new', function(req, res) {
+app.get('/sample-node-mongo-app/employee/new', function(req, res) {
     res.render('employee_new', {
         title: 'New Employee'
     });
 });
 
 //save new employee
-app.post('/employee/new', function(req, res){
+app.post('/sample-node-mongo-app/employee/new', function(req, res){
     employeeProvider.save({
         title: req.param('title'),
         name: req.param('name')
     }, function( error, docs) {
-        res.redirect('/')
+        res.redirect('/sample-node-mongo-app/')
     });
 });
 
 //update an employee
-app.get('/employee/:id/edit', function(req, res) {
+app.get('/sample-node-mongo-app/employee/:id/edit', function(req, res) {
 	employeeProvider.findById(req.param('_id'), function(error, employee) {
 		res.render('employee_edit',
 		{ 
@@ -103,19 +103,19 @@ app.get('/employee/:id/edit', function(req, res) {
 });
 
 //save updated employee
-app.post('/employee/:id/edit', function(req, res) {
+app.post('/sample-node-mongo-app/employee/:id/edit', function(req, res) {
 	employeeProvider.update(req.param('_id'),{
 		title: req.param('title'),
 		name: req.param('name')
 	}, function(error, docs) {
-		res.redirect('/')
+		res.redirect('/sample-node-mongo-app/')
 	});
 });
 
 //delete an employee
-app.post('/employee/:id/delete', function(req, res) {
+app.post('/sample-node-mongo-app/employee/:id/delete', function(req, res) {
 	employeeProvider.delete(req.param('_id'), function(error, docs) {
-		res.redirect('/')
+		res.redirect('/sample-node-mongo-app/')
 	});
 });
 
