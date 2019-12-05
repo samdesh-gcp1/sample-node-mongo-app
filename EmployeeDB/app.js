795298dcca05a6cf66fad08f68a2f3361e9ae5d4
samdesh-gcp1 <58552071+samdesh-gcp1@users.noreply.github.com>
2019-12-05 17:06:26 +0530

    Update app.js

--- EmployeeDB/app.js
+++ EmployeeDB/app.js
@@ -60,7 +60,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-var employeeProvider= new EmployeeProvider('localhost', 27017);
+var employeeProvider= new EmployeeProvider('mongo', 27017);
 
 //Routes
 
